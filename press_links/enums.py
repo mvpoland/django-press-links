3e5e38cb37e6a2bb752159f8c44b6222da9cbfa2
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-08-21 17:10:11 +0200

    use translations from django and not our own

--- press_links/enums.py
+++ press_links/enums.py
@@ -1,3 +1,4 @@
+from django.utils.dates import MONTHS
 from django.utils.translation import ugettext_lazy as _
 
 LIVE_STATUS = 1
@@ -9,5 +10,6 @@ STATUS_CHOICES = (
     (HIDDEN_STATUS, _('Hidden')),
 )
 
-MONTH_NAMES = ('', _('January'), _('Feburary'), _('March'), _('April'), _('May'), _('June'), _('July'), _('August'), _('September'), _('October'), _('November'), _('December'))
-
+# We're using the default translations from Django here by importing MONTHS and refer to it
+MONTH_NAMES = ('', MONTHS[1], MONTHS[2], MONTHS[3], MONTHS[4], MONTHS[5], MONTHS[6], MONTHS[7], MONTHS[8], MONTHS[9],
+               MONTHS[10], MONTHS[11], MONTHS[12])
