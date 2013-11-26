6ad77ddd02783aa143767a9d280f9baa6e987ca1
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-11-26 18:17:22 +0100

    pepify

--- press_links/models.py
+++ press_links/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
-from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from tinymce import models as tinymce_models
 from press_links.enums import STATUS_CHOICES, LIVE_STATUS, DRAFT_STATUS
 from datetime import datetime
+
 
 class EntryManager(models.Manager):
     def live(self):
@@ -15,6 +15,7 @@ class EntryManager(models.Manager):
         :rtype: django.db.models.QuerySet
         """
         return self.filter(pub_date__lte=datetime.now(), status=LIVE_STATUS).filter(site=Site.objects.get_current())
+
 
 class Entry(models.Model):
     author = models.ForeignKey(User, verbose_name=_('author'), related_name='%(app_label)s_%(class)s_related')
@@ -40,6 +41,7 @@ class Entry(models.Model):
 
     def __unicode__(self):
         return self.title
+
 
 class Link(models.Model):
     link = models.CharField(max_length=255, verbose_name=_('link address (add http:// for external link)'))
